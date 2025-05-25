bf2d6151e56f27bab840c48948df97338ecceb7e
Sam.Robin <samuelrobin156@gmail.com>
2025-05-25 22:57:59 +1200

    Update: Student Forms

--- app/src/main/java/com/example/uspenrolme/student/applications/StudentFormsFragment.java
+++ app/src/main/java/com/example/uspenrolme/student/applications/StudentFormsFragment.java
@@ -9,30 +9,34 @@ import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import com.example.uspenrolme.R;
 
-public class StudentApplicationsFragment extends Fragment {
+public class StudentFormsFragment extends Fragment {
 
     Button trackApplicationsBtn;
     Button notificationsBtn;
     Button gradeRecheckBtn;
-    Button studentFormsBtn;
-    
+    Button compassionateFormBtn;
+    Button graduationApplicationsBtn;
 
-    public StudentApplicationsFragment() {
-        super(R.layout.fragment_applications);
+    public StudentFormsFragment() {
+        super(R.layout.fragment_student_forms);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_applications, container, false);
+        View view = inflater.inflate(R.layout.fragment_student_forms, container, false);
 
         trackApplicationsBtn = view.findViewById(R.id.track_application_btn);
         notificationsBtn = view.findViewById(R.id.application_notification_btn);
         //gradeRecheckBtn = view.findViewById(R.id.grade_recheck_btn);
-        studentFormsBtn = view.findViewById(R.id.student_forms_btn);
+        graduationApplicationsBtn = view.findViewById(R.id.graduation_form_btn);
+        compassionateFormBtn = view.findViewById(R.id.compassionate_form_btn);
+
+
+        compassionateFormBtn.setOnClickListener(v -> openFragment(new CompassionateAegrotatApplicationFragment()));
 
         // Example: open grade recheck fragment
         //gradeRecheckBtn.setOnClickListener(v -> openFragment(new ApplicationGradeReCheckFragment()));
-        studentFormsBtn.setOnClickListener(v -> openFragment(new StudentFormsFragment()));
+        graduationApplicationsBtn.setOnClickListener(v -> openFragment(new GraduationApplicationFragment()));
         // Add other button listeners as needed
 
         return view;
